c4a10f465476bbcf77c0e46a74bf0b4866691a83
Taewoong Moon <moondingie5@gmail.com>
2019-02-11 00:24:56 +0900

    async await

--- src/App.js
+++ src/App.js
@@ -7,37 +7,30 @@ class App extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "극한직업",
-            imgUrl: "http://www.moviechart.co.kr/thumb?type=1&width=700&height=500&m_code=20182530&source=http://www.moviechart.co.kr/assets/upload/poster/190117101529_6201.jpg",
-          },
-          {
-            title: "인생후르츠",
-            imgUrl: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81367/81367_1000.jpg",
-          },
-          {
-            title: "우리들",
-            imgUrl: "http://ojsfile.ohmynews.com/STD_IMG_FILE/2016/0707/IE001987882_STD.jpg",
-          },
-          {
-            title: "아바타",
-            imgUrl: "https://s-i.huffpost.com/gen/5166168/thumbs/o-THE-570.jpg?7",
-          }
-        ]
-      })
-    }, 2000)
+    this._getMovies();
   }
 
   _renderMovies = () => {
     const movies = this.state.movies.map((movie, index) => {
       return (
-        <Movie movieTitle={movie.title} movieImg={movie.imgUrl} key={index} />
+        <Movie movieTitle={movie.title} movieImg={movie.large_cover_image} key={index} />
       )
     })
     return movies;
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
+    .then(data => data.json())
+    .then(json => json.data.movies)
+    .catch(err => console.error(err))
   }
 
   render() {
